3bb0ceaaf10d73118f7a01fd093cf74921434bbe
lukasz.gawron <lukasz.gawron@performgroup.com>
2019-01-03 17:02:35 +0100

    initial scala maven project with scala, scalatest and akka streams

---
+++ src/main/scala/pl/laron/tutorial/akka/streams/App.scala
@@ -0,0 +1,7 @@
+package pl.laron.tutorial.akka.streams
+
+object App {
+  def main(args: Array[String]): Unit = {
+    println("works")
+  }
+}
---
+++ src/test/scala/pl/laron/tutorial/akka/streams/AppTest.scala
@@ -0,0 +1,9 @@
+package pl.laron.tutorial.akka.streams
+
+import org.scalatest.FunSpec
+
+class AppTest extends FunSpec {
+  it("should run") {
+    assert(true)
+  }
+}
