f010ec3532988d69283cb495b2f00d62ff76b5f7
lukasz.gawron <lukasz.gawron@performgroup.com>
2019-09-18 10:02:00 +0200

    add assertion explaining where scheduled job is accessible in scheduler instance

--- src/test/scala/pl/laron/tutorial/akka/QuartzSchedulerTest.scala
+++ src/test/scala/pl/laron/tutorial/akka/QuartzSchedulerTest.scala
@@ -1,17 +1,17 @@
 package pl.laron.tutorial.akka
 
-import java.time.{Instant, ZoneId}
 import java.time.format.DateTimeFormatter
+import java.time.{Instant, ZoneId}
 import java.util.TimeZone
-import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
 import akka.testkit.{TestKit, TestProbe}
 import com.typesafe.akka.extension.quartz.QuartzSchedulerExtension
 import org.scalatest.FunSpecLike
 
-class QuartzSchedulerTest extends TestKit(ActorSystem("QuartzSchedulerTest")) with FunSpecLike {
-  private val scheduler = QuartzSchedulerExtension(system)
+class QuartzSchedulerTest extends TestKit(ActorSystem("QuartzSchedulerTest")) with FunSpecLike{
+  //scheduler is instantiated once per actor system, so shared by all tests
+  val scheduler: QuartzSchedulerExtension = QuartzSchedulerExtension(system)
   private val CronFormat: DateTimeFormatter =
     DateTimeFormatter.ofPattern("ss mm HH dd MM ? yyyy").withZone(ZoneId.of("UTC"))
 
@@ -20,6 +20,7 @@ class QuartzSchedulerTest extends TestKit(ActorSystem("QuartzSchedulerTest")) wi
   }
 
   it("start based on schedule in future") {
+
     val probe = TestProbe()
     scheduler
       .createSchedule(
@@ -30,7 +31,8 @@ class QuartzSchedulerTest extends TestKit(ActorSystem("QuartzSchedulerTest")) wi
         timezone = TimeZone.getTimeZone("UTC"))
 
     scheduler.schedule(name = "test1", receiver = probe.ref, msg = Ping)
-
+    assert(scheduler.runningJobs.size == 1)
+    assert(scheduler.schedules.size == 1)
     probe.expectMsg(Ping)
   }
 
