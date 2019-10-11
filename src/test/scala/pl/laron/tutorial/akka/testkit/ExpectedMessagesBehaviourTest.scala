83a4a022c01805b08a0d37740c131fc11abd0855
lukasz.gawron <lukasz.gawron@performgroup.com>
2019-10-11 08:53:02 +0200

    tests confirming that expectedMsg works for all messages received until its called plus timeout duration from its invocation (default 3 seconds timeout)

---
+++ src/test/scala/pl/laron/tutorial/akka/testkit/ExpectedMessagesBehaviourTest.scala
@@ -0,0 +1,28 @@
+package pl.laron.tutorial.akka.testkit
+
+import akka.actor.ActorSystem
+import akka.testkit.{TestKit, TestProbe}
+import org.scalatest.FunSpecLike
+import pl.laron.tutorial.akka.quartz.Ping
+
+class ExpectedMessagesBehaviourTest extends TestKit(ActorSystem("ExpectedMessagesBehaviourTest")) with FunSpecLike {
+  // implementation of expectedMsg works on queue-like structure maintaining messages
+  // received by actor in chronological order, once message is retreived from queue,
+  // is not available for other asserts anymore
+  it("TestProbe receives messages send to probe BEFORE expected calls are done") {
+    val probe = TestProbe()
+    probe.ref ! Ping
+
+    probe.expectMsg(Ping)
+  }
+
+  it("removes already asserted msg from internal queue so other asserts won't work on already processed msg") {
+    val probe = TestProbe()
+    probe.ref ! Ping
+
+    probe.expectMsg(Ping)
+    assertThrows[AssertionError] {
+      probe.expectMsg(Ping)
+    }
+  }
+}
